35fc41ce08ff84e083fe2cf98e29a2a67cc5c4d2
martin hermant <martin.hermant@gmail.com>
2015-09-10 22:54:20 +0200

    ok xcodeTst

--- generate.py
+++ generate.py
@@ -1,70 +1,134 @@
 from utils.algorithms_info import *
 import os
 import sys
+import re
 
 from openstep_parser import *
 
 
-def getTarget(x):
+def getTarget(x,name):
     root = x["rootObject"]
     targets = x["objects"][root]["targets"]
     for t in targets:
-            if x["objects"][t]["name"] == "essentiaX":
+            if x["objects"][t]["name"] == name:
                 print "found"
                 return t;
     print "no target found"
 
-def getBuildFiles(x,t):
+
+def getBuildRef(x,t):
     res = []
+    for b in x["objects"][t]["buildPhases"]:
+        if x["objects"][b]["isa"] == "PBXSourcesBuildPhase":
+            res+=[b]
+    return res
+
+def getBuildFileRef(x,t):
     ref = []
     for b in x["objects"][t]["buildPhases"]:
         if x["objects"][b]["isa"] == "PBXSourcesBuildPhase":
             print "found build : "+str(b)
             for hf in x["objects"][b]["files"]:
                 ref+=[x["objects"][hf]["fileRef"]]
+    return ref
 
-    
+def getPathFromRef(x,ref):
+    res = []
     for hf in ref:
-        res += [x["objects"][hf]["path"] ]
+        if hf in x["objects"]:
+            r = hf
+            if "fileRef" in x["objects"][hf]:
+                r= x["objects"][hf]["fileRef"] 
+            res += [x["objects"][r]["path"] ]
     return res
 
 
 
+
 def getDependencies(a,res,algos,rootDir):
-    
+    # print "getDependencies for "+a
     with open(rootDir + "/" +algos[a]["source"]) as file:
         lines = file.readlines()
-
     newdep = []
     for l in lines:
         for al in algos:  
             if "\""+al+"\"" in l and al != a:
                 newdep+=[al]
     newdep = list(set(newdep))
-    
-        
-
     for d in newdep:
         getDependencies(d,res,algos,rootDir)
-        del algos[d]
+        # del algos[d]
         res+=[d]
+
+    del algos[a]
     return res;
 
 
 
 
+def pruneXcode(xc,t,toRemove,xcodeFile):
+
+    files =  getBuildFileRef(xc,t);
+    with open(xcodeFile) as file:
+        lines = file.readlines()
+
+    inSection = False
+    build = getBuildRef(xc, t)[0]
+
+    srcToRm = [x["source"].split('/')[-1] for x in toRemove.itervalues()]
+
+    print srcToRm
+
+
+    ltodelete = []
+    i = 0
+    for l in lines:
+        if len(l)>2:
+            
+            if inSection:
+                if "};" in l:
+                    inSection = False
+                
+                ref = str(re.split('\W+', l)[1])
+                if ref and ref[0].isdigit():
+                    curS = getPathFromRef(xc, [ref])
+                    
+                    if curS[0] in srcToRm:
+                        ltodelete+=[i]
+                        i-=1
+                        print "remove : "+str(i)+" "+str(curS)
+            if build  in l and l[-2]=='{':
+                inSection = True
+        i+=1
+
+
+    for lt in ltodelete:
+        # pass
+        del lines[lt]
+
+    with open(xcodeFile + "2",'w') as file:
+        file.writelines(lines)
+   
+
 
 if __name__ == '__main__':
     
-  
+    env =  os.environ
+    pruneIt = []
+    
+    if "ESSENTIA_PRUNE" in env:
+        pruneIt = os.environ["ESSENTIA_PRUNE"].split(' ')
+    print "pruning for algos : " + str(pruneIt)
+
+
     essentiasrc = sys.argv[1]
     xcodeFile = sys.argv[2]+"/project.pbxproj"
     with open(xcodeFile) as file:
-
         xcode =  OpenStepDecoder.ParseFromFile(file);
 
-        t = getTarget(xcode)
-        files = getBuildFiles(xcode, t)
+        t = getTarget(xcode,"essentiaX")
+        files = getPathFromRef(xcode,getBuildFileRef(xcode, t))
+
 
     algodir = os.path.join(essentiasrc,"algorithms")    
     # create_version_h();
@@ -73,10 +137,6 @@ if __name__ == '__main__':
     print algos
     res = []
     
-    algos2 = algos.copy()
-    print getDependencies("LowLevelSpectralExtractor", res, algos2, essentiasrc)
-
-
 
 
 
@@ -91,6 +151,23 @@ if __name__ == '__main__':
 
     for i in ignore:
         del algos[i]
+
+
+    if pruneIt:
+        toRemove = algos.copy()
+        for a in pruneIt:
+            getDependencies(a, res, toRemove, essentiasrc)
+            
+        
+        del toRemove["IFFTA"]
+        del toRemove["FFTA"]
+        
+        pruneXcode(xcode, getTarget(xcode,"essentiaXLight"),toRemove,xcodeFile)
+        rstr = []
+        for r in toRemove:
+            rstr+=[r]
+        print "you can remove "+ str(rstr)
+        
 
     regFile = os.path.join(algodir,"essentia_algorithms_reg.cpp")
     create_registration_cpp(algos,regFile)
